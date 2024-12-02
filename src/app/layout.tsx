ec085a94dacf8aa071235cb1521f778752d29d0e
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-12-02 08:17:15 -0300

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,42 @@
+import "./globals.css";
+
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+
+export const metadata: Metadata = {
+  title: "solo un chico tranquilo",
+  description: "Sólo un generador de tipos tranquilos",
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    title: "solo un chico tranquilo",
+    description: "Sólo un generador de tipos tranquilos.",
+    url: "https://guy-chill.vercel.app",
+    images: ["opengraph-image.png"],
+    type: "website",
+    locale: "pt_BR",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR" className={inter.variable} suppressHydrationWarning>
+      <body className="antialiased overflow-hidden">
+        <div className="fixed left-0 top-0 -z-10  h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]" />
+
+        {children}
+      </body>
+    </html>
+  );
+}
