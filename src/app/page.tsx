35733b14b073b0567f66af3f8a0238c51559be7a
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-12-02 09:00:07 -0300

    fix links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { useFabric } from "@/hooks/use-fabric";
 
 import { Toolbar } from "@/components/toolbar";
@@ -19,25 +20,38 @@ export default function HomePage() {
 
   return (
     <>
-      <div className="px-2 space-y-10 min-h-screen items-center h-full flex-col flex justify-between">
+      <div className='px-2 space-y-10 min-h-screen items-center h-full flex-col flex justify-between'>
         <div>
-          <footer className="text-center p-4">
-          <p>By Kevin Ayerdi Perticarari</p>
-          <div>
-            <a href="https://github.com/KAyerdi" target="_blank" rel="noopener noreferrer" className="mr-4 rounded-full bg-gray-200 px-4 py-2 hover:bg-gray-300 transition">
-              GitHub
-            </a>
-            <a href="https://www.linkedin.com/in/kevin-perticarari-ayerdi-94968412b/" target="_blank" rel="noopener noreferrer" className="rounded-full bg-gray-200 px-4 py-2 hover:bg-gray-300 transition">
-              LinkedIn
-            </a>
-          </div>
-          <div><a href="https://soykevin.ar">SoyKevin.ar</a></div>
-        </footer></div>
+          <footer className='text-center p-4'>
+            <p>By Kevin Ayerdi Perticarari</p>
+            <div className='flex justify-center space-x-4'>
+              <a
+                href='https://github.com/KAyerdi'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='rounded-full bg-gray-200 hover:bg-gray-300 transition px-4 py-2'
+              >
+                GitHub
+              </a>
+              <a
+                href='https://www.linkedin.com/in/kevin-perticarari-ayerdi-94968412b/'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='rounded-full bg-gray-200 hover:bg-gray-300 transition px-4 py-2'
+              >
+                LinkedIn
+              </a>
+            </div>
+            <div>
+              <a href='https://soykevin.ar'>SoyKevin.ar</a>
+            </div>
+          </footer>
+        </div>
         <canvas
           ref={canvasRef}
-          className="border rounded-3xl overflow-hidden "
+          className='border rounded-3xl overflow-hidden '
         />
-        <div className="pt-10 pb-8 space-y-5 flex items-center flex-col">
+        <div className='pt-10 pb-8 space-y-5 flex items-center flex-col'>
           <Toolbar
             setBackgroundImage={setBackgroundImage}
             addText={addText}
